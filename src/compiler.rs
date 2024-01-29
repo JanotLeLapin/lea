945e4a32e385ba1b3f23ed7d5475f58371af7f0f
Joseph DALY <joseph300905@gmail.com>
2024-01-29 19:23:04 +0100

    make class inherit Object

--- src/compiler.rs
+++ src/compiler.rs
@@ -2,6 +2,11 @@ use crate::Rule;
 
 use bytes::BufMut;
 
+pub mod access {
+    pub const PUBLIC: u16 = 0x0001;
+    pub const SUPER: u16 = 0x0020;
+}
+
 #[derive(Debug)]
 pub enum CompileError {
     ExpectedModule,
@@ -23,16 +28,23 @@ pub fn compile<'a>(ast: &mut pest::iterators::Pairs<'a, Rule>) -> Result<Vec<u8>
     res.put_u16(0);
     res.put_u16(34);
 
-    res.put_u16(3);
+    res.put_u16(5); // cp size
+
     res.put_u8(7);
     res.put_u16(2);
     res.put_u8(1);
     res.put_u16(module.len() as u16);
     res.put_slice(module.as_bytes());
 
-    res.put_u16(21);
+    res.put_u8(7);
+    res.put_u16(4);
+    res.put_u8(1);
+    res.put_u16(16);
+    res.put_slice(b"java/lang/Object");
+
+    res.put_u16(access::PUBLIC | access::SUPER);
     res.put_u16(1);
-    res.put_u16(0);
+    res.put_u16(3);
     res.put_u64(0);
 
     Ok(res.to_vec())
