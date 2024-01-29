da871a984896886cc7b2023d5e9395aada9c1e46
Joseph DALY <joseph300905@gmail.com>
2024-01-29 21:15:35 +0100

    convenient ClassFile trait

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,6 +1,33 @@
 use crate::Rule;
 
-use bytes::BufMut;
+use bytes::{Buf, BufMut, BytesMut};
+
+enum Constant<'a> {
+    Class(u16),
+    UTF8(&'a str),
+}
+
+trait ClassFile {
+    fn put_constant(&mut self, constant: Constant<'_>);
+}
+
+impl ClassFile for BytesMut {
+    fn put_constant(&mut self, constant: Constant<'_>) {
+        use Constant::*;
+
+        match constant {
+            Class(idx) => {
+                self.put_u8(7);
+                self.put_u16(idx);
+            },
+            UTF8(slice) => {
+                self.put_u8(1);
+                self.put_u16(slice.len() as u16);
+                self.put_slice(slice.as_bytes());
+            }
+        }
+    }
+}
 
 pub mod access {
     pub const PUBLIC: u16 = 0x0001;
@@ -14,6 +41,7 @@ pub enum CompileError {
 
 pub fn compile<'a>(ast: &mut pest::iterators::Pairs<'a, Rule>) -> Result<Vec<u8>, CompileError> {
     use CompileError::*;
+    use Constant::*;
 
     let module = match ast.next() {
         Some(pair) => match pair.as_rule() {
@@ -23,24 +51,18 @@ pub fn compile<'a>(ast: &mut pest::iterators::Pairs<'a, Rule>) -> Result<Vec<u8>
         _ => None,
     }.ok_or(ExpectedModule)?;
 
+    let cp = vec![
+        Class(2), UTF8(module),
+        Class(4), UTF8("java/lang/Object"),
+    ];
+
     let mut res = bytes::BytesMut::new();
     res.put_u32(0xCAFEBABE);
     res.put_u16(0);
     res.put_u16(52);
 
-    res.put_u16(5); // cp size
-
-    res.put_u8(7);
-    res.put_u16(2);
-    res.put_u8(1);
-    res.put_u16(module.len() as u16);
-    res.put_slice(module.as_bytes());
-
-    res.put_u8(7);
-    res.put_u16(4);
-    res.put_u8(1);
-    res.put_u16(16);
-    res.put_slice(b"java/lang/Object");
+    res.put_u16(cp.len() as u16 + 1);
+    for constant in cp { res.put_constant(constant); };
 
     res.put_u16(access::PUBLIC | access::SUPER);
     res.put_u16(1);
