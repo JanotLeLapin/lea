e98137e2ca151c3c8da887e596521b706f1ca0cc
Joseph DALY <joseph300905@gmail.com>
2024-02-04 13:01:08 +0100

    more unit tests

--- test/Main.java
+++ test/Main.java
@@ -6,5 +6,7 @@ public class Main {
   @Test public void return_statement() {
     assertEquals("bar", Tests.basic_return());
     assertEquals("baz", Tests.complex_return());
+    assertEquals(5, Tests.num_return());
+    assertEquals(false, Tests.bool_return());
   }
 }
